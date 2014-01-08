5d904a03f52b4005356b48e7f6a0a28e10ec7ef3
Laurent Pouilloux <lolo@pouilloux.org>
2014-01-08 10:07:52 +0100

    [lib] remove ip list file in wait_vms_have_started

--- src/vm5k/actions.py
+++ src/vm5k/actions.py
@@ -204,6 +204,7 @@ def wait_vms_have_started(vms, host = None):
         if not ssh_open:
             logger.info(str(nmap_tries)+': '+  started_vms+'/'+str(len(vms)) )
     SshProcess('rm '+tmpfile[1].split('/')[-1], host, connection_params = {'user': user}).run()    
+    Process('rm'+tmpfile[1]).run()
     if ssh_open:
         logger.info('All VM have been started')
         return True
