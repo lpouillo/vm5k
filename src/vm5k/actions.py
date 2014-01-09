e99de5ccc2d424d92268829ec893f1687626e266
Laurent Pouilloux <lolo@pouilloux.org>
2014-01-09 14:05:08 +0100

    [lib] cleaning

--- src/vm5k/actions.py
+++ src/vm5k/actions.py
@@ -36,9 +36,6 @@ def show_vms(vms):
         ', '.join( [style.VM(vm['id'])+' ('+str(vm['mem'])+'Mb, '+str(vm['n_cpu'])+' cpu '+\
                    vm['cpuset']+', '+str(vm['hdd'])+'Gb)' 
                     for vm in vms ] ) )
-
-#def _default_xml_value(key):
-#            return default_vm[key] if key not in vm.attrib else vm.get(key)
             
 
 def define_vms( vms_id, template = None, ip_mac = None, state = None, host = None,
