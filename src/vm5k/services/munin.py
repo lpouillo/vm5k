3986bc0b16619963e9c626026c3766cb40533cd0
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2014-01-31 14:05:18 +0100

    [lib] start reimplementation of munin service

--- src/vm5k/services/munin.py
+++ src/vm5k/services/munin.py
@@ -15,43 +15,56 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Execo.  If not, see <http://www.gnu.org/licenses/>
+from os import fdopen
 from tempfile import mkstemp
 from execo import logger, SshProcess, Process, Put
 from execo.log import style
 
 
-def munin_server(server, clients, plugins = [ 'cpu', 'memory', 'iostat']):
+
+def _munin_clients(server, clients, plugins = [ 'cpu', 'memory', 'iostat']):
+    """ """
+
+
+
+def _munin_server(server, clients):
     """Install the monitoring service munin. Must be executed inside Grid'5000
     to be able to resolve the server and clients IP.
-    
+
     :param server: a execo.Host
-    
+
     :param clients: a list of execo.Hosts
-    
+
     :param plugins: a list of munin plugins
-    
+
     """
     logger.info('Munin monitoring service installation, server = %s, clients = \n %s',
                 server.address, [host.address for host in clients])
-    
+
+    logger.debug('Configuring munin server %s', style.host('server'))
+    cmd = 'export DEBIAN_MASTER=noninteractive ; apt-get update && apt-get install -y munin'
+    inst_munin_server = SshProcess(cmd, server).run()
+
     logger.debug('Creating configuration files for server')
-    _, server_conf = mkstemp(dir = '/tmp/', prefix='munin-nodes_')
-    f = open(server_conf, 'w')
+    fd, server_conf = mkstemp(dir = '/tmp/', prefix='munin-nodes_')
+    f = fdopen(fd, 'w')
     for host in clients:
         get_ip = Process('host '+host.address).run()
         ip =  get_ip.stdout.strip().split(' ')[3]
         f.write('['+host.address+']\n    address '+ip+'\n   use_node_name yes\n\n')
     f.close()
-            
-    logger.debug('Configuring munin server %s', style.host('server'))
-    cmd = 'export DEBIAN_MASTER=noninteractive ; apt-get update && apt-get install -y munin'
-    inst_munin_server = SshProcess(cmd, server).run()
+
+    Put([server], [server_conf], remote_location='/etc/').run()
+    SshProcess('cd /etc && cp '+server_conf.split('/')[-1]+' munin.conf', server).run()
+    Process('rm '+server_conf).run()
+
+def setup_munin(server, clients, plugins = [ 'cpu', 'memory', 'iostat']):
 
 
 def get_munin_stats(server, destination_directory = '.'):
     """Retrieve the munin statistics """
-    logger.error('Not implemented')
-    
-    
-def add_munin_plugin(plugin, host):
+
+
+
+def add_munin_plugins(plugins, hosts):
     """ """
