720816dde01b4c6dc8fa73c5f504547de6e95ab8
Laurent Pouilloux <lolo@pouilloux.org>
2014-02-25 13:52:51 +0100

    [bin] change script title [examples] add hist plot in boot_time

--- examples/boot_time.py
+++ examples/boot_time.py
@@ -3,11 +3,12 @@ import os
 import time
 import datetime
 from numpy import array, median
-from execo import sleep, TaktukRemote, logger
-from vm5k import print_step
-sleep(2)
+import matplotlib.pyplot as plt
+from execo import TaktukRemote, logger
+
 logger.info('Measuring boot time')
 
+# Reading VMs list
 vms = []
 run_dir = 'boot_time/'
 
@@ -18,14 +19,13 @@ for line in f:
         vms.append({'id': tmp[1], 'ip': tmp[0]})
 f.close()
 
+# Measuring boot_duration
 now = time.time()
-
 get_uptime = TaktukRemote('cat /proc/uptime', [vm['ip']
                     for vm in vms]).run()
 boot_time = {}
 for p in get_uptime.processes:
     boot_time[p.host.address] = now - float(p.stdout.strip().split(' ')[0])
-
 
 get_ssh_up = TaktukRemote('grep listening /var/log/auth.log' + \
             ' |grep 0.0.0.0|awk \'{print $1" "$2" "$3}\'',
@@ -38,7 +38,7 @@ for p in get_ssh_up.processes:
     boot_duration.append(ssh_up - boot_time[p.host.address])
 
 
-#boot_time
+# Calculating stats
 uptime = array(boot_duration)
 dur_min = uptime.min()
 dur_max = uptime.max()
@@ -49,3 +49,11 @@ logger.info('VMS: %s', len(vms))
 logger.info('Average: %s', mean)
 logger.info('Standard deviation: %s', std)
 logger.info('Min-max: %s-%s', dur_min, dur_max)
+
+# Drawing historgram
+n, bins, patches = plt.hist(uptime, 5, normed=1, facecolor='g', alpha=0.75)
+plt.xlabel('Boot duration')
+plt.ylabel('Number of VMs')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.savefig('boot_time.png')
