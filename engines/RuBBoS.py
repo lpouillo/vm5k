019a6ba20676a3e1100ff0e8fc3dd9ac10af5ae9
Laurent Pouilloux <lolo@pouilloux.org>
2014-04-08 16:19:37 +0200

    [engines] rubbos: - change remote_dest -> remote_file in services - add log_files in services - configure and start services - configure and start stress

--- engines/RuBBoS.py
+++ engines/RuBBoS.py
@@ -12,7 +12,7 @@ class RuBBoS(vm5k_engine_para):
     def __init__(self):
         super(RuBBoS, self).__init__()
         self.env_name = 'wheezy-x64-base'
-        self.stress_time = 600
+        self.stress_time = 30
         self.nb_client = 1
         self.n_nodes = 4
 
@@ -85,7 +85,7 @@ class RuBBoS(vm5k_engine_para):
             destroy_vms(hosts)
 
             n_vm = comb['HTTP'] + comb['App'] + comb['DB'] + 4
-            logger.info(thread_name + ': Defining %s virtual machines', n_vm)
+            logger.info(thread_name + 'Defining %s virtual machines', n_vm)
 
             # Defining load balancers and client virtual machines
             ids = ['lb-http', 'lb-app', 'lb-db', 'client']
@@ -154,19 +154,19 @@ class RuBBoS(vm5k_engine_para):
                         backing_file=backing_files)
             logger.detail('VMS %s ', pformat(vms))
             # Create disks, install vms and boot by core
-            logger.info(thread_name + ': Creating disks')
+            logger.info(thread_name + 'Creating disks')
             create = create_disks(vms).run()
             if not create.ok:
                 logger.error(thread_name + ': Unable to create the VMS disks')
                 exit()
 
-            logger.info(thread_name + ': Installing VMS')
+            logger.info(thread_name + 'Installing VMS')
             install = install_vms(vms).run()
             if not install.ok:
                 logger.error(thread_name + ': Unable to install the VMS')
                 exit()
 
-            logger.info(thread_name + ': Booting VMs')
+            logger.info(thread_name + 'Booting VMs')
             boot_successfull = boot_vms_list(vms)
             if not boot_successfull:
                 logger.error(thread_name + ': Unable to boot all the VMS')
@@ -187,7 +187,7 @@ class RuBBoS(vm5k_engine_para):
                 'lb-http':
                     {'func': generate_http_proxy,
                      'template': 'default_http_lb',
-                     'remote_dest': '/etc/apache2/site-available/',
+                     'remote_file': '/etc/apache2/sites-available/default',
                      'launch_cmd': '/etc/init.d/apache2 restart',
                      'member_vms': filter(lambda x: 'http-' in x['id'], vms),
                      'log_files': ['/var/log/apache2/access.log',
@@ -195,7 +195,7 @@ class RuBBoS(vm5k_engine_para):
                 'lb-app':
                     {'func': generate_tomcat_proxy,
                      'template': 'default_tomcat_lb',
-                     'remote_dest': '/etc/apache2/site-available/',
+                     'remote_file': '/etc/apache2/sites-available/default',
                      'launch_cmd': '/etc/init.d/apache2 restart',
                      'member_vms': filter(lambda x: 'app-' in x['id'], vms),
                      'log_files': ['/var/log/apache2/access.log',
@@ -203,14 +203,14 @@ class RuBBoS(vm5k_engine_para):
                 'lb-db':
                     {'func': generate_db_proxy,
                      'template': 'haproxy.cfg',
-                     'remote_dest': '/etc/haproxy/',
+                     'remote_file': '/etc/haproxy/haproxy.cfg',
                      'launch_cmd': 'haproxy -f /etc/haproxy/haproxy.cfg',
                      'member_vms': filter(lambda x: 'db-' in x['id'], vms),
                      'log_files': None},
                 'http-':
                     {'func': generate_http,
                      'template': 'default_http',
-                     'remote_dest': '/etc/apache2/site-available/',
+                     'remote_file': '/etc/apache2/sites-available/default',
                      'launch_cmd': '/etc/init.d/apache2 restart',
                      'member_vms': filter(lambda x: 'lb-app' in x['id'], vms),
                      'log_files': ['/var/log/apache2/access.log',
@@ -218,153 +218,81 @@ class RuBBoS(vm5k_engine_para):
                 'app-':
                     {'func': generate_app,
                      'template': 'mysql.properties',
-                     'remote_dest': '/var/www/',
+                     'remote_file': '/var/www/mysql.properties',
                      'launch_cmd': '/etc/init.d/tomcat6 restart',
                      'member_vms': filter(lambda x: 'lb-db' in x['id'], vms),
                      'log_files': ['/var/lib/tomcat6/logs/rubbos.log']},
                 'db-':
                     {'func': None,
                      'template': None,
-                     'remote_dest': None,
+                     'remote_file': None,
                      'launch_cmd': None,
-                     'member_vms': None},
+                     'member_vms': None,
+                     'log_files': None},
                 'client':
                     {'func': generate_client,
                      'template': 'rubbos.properties',
-                     'remote_dest': '/root/RUBBoS/Client/',
+                     'remote_file': '/root/RUBBoS/Client/rubbos.properties',
                      'launch_cmd': 'cd /root/RUBBoS/Client/ && ' + \
                      'java -Xmx256m -Xms128m -server -classpath . ' + \
                      'edu.rice.rubbos.client.ClientEmulator',
-                     'member_vms': filter(lambda x: 'lb-' in x['id'], vms)}
+                     'member_vms': filter(lambda x: 'lb-' in x['id'], vms),
+                     'log_files': ['/root/RUBBoS/bench/']}
                     }
 
             for service, conf in services.iteritems():
                 if conf['template']:
-                    print service
-                    print conf['member_vms']
+                    service_vms = map(lambda y: y['ip'],
+                        filter(lambda x: service in x['id'], vms))
+                    logger.detail('service: %s %s', ','.join(service_vms),
+                                ','.join([vm['id'] for vm in conf['member_vms']]))
                     f_template = open('conf_template/' + conf['template'])
-                    fd, outfile = mkstemp(dir='/tmp/', prefix='http-lb_')
+                    fd, outfile = mkstemp(dir='/tmp/', prefix=service + '_')
                     f = fdopen(fd, 'w')
                     conf['func'](f, f_template, conf['member_vms'])
                     f_template.close()
-                    print 'conf_genrated in ' + outfile
-                    put_file = Put(map(lambda y: y['ip'],
-                            filter(lambda x: service in x['id'], vms)),
-                        [outfile], remote_location=conf['remote_dest'],
-                        connection_params={'user': 'root'})
-                    print put_file
-            # Starting services
+                    logger.detail('conf generated in ' + outfile)
+                    logger.setLevel('DEBUG')
+                    put_file = Put(service_vms, [outfile]).run()
+                    if not put_file.ok:
+                        exit()
+                    Remote('cp ' + outfile.split('/')[-1] + ' ' + conf['remote_file'],
+                        service_vms).run()
+
+            # Starting services sequentially
             for service in ['lb-db', 'app-', 'lb-app', 'http-', 'lb-http']:
                 conf = services[service]
-                print filter(lambda x: service in x['id'], vms)
-                print map(lambda y: y['ip'],
-                           filter(lambda x: service in x['id'], vms))
-                SshProcess(conf['launch_cmd'], map(lambda y: y['ip'],
+                launch = Remote(conf['launch_cmd'], map(lambda y: y['ip'],
                            filter(lambda x: service in x['id'], vms))).run()
+                for p in launch.processes:
+                    print p.host
+                    print p.stdout
+                    print p.stderr
 
-#             for vm in vms:
-#                     Put(vm['ip'], [tmp_dir + "default_http_lb"]).run()
-#                     SshProcess('mv /root/default_http_lb /etc/apache2/site-available/', vm['ip']).run()
-#                 elif "vm-app-lb" == vm['id']:
-#                     Put(vm['ip'], [tmp_dir + "default_tomcat_lb"]).run()
-#                     SshProcess('mv /root/default_tomcat_lb /etc/apache2/site-available/', vm['ip']).run()
-#                 elif "vm-db-lb" == vm['id']:
-#                     
-#                     Put(vm['ip'], [tmp_dir + "haproxy.cfg"] ).run()
-#                     SshProcess('mv /root/haproxy.cfg /etc/', vm['ip']).run()
-#                 elif "vm-http" in vm['id']:
-#                     grep("conf_template/default_http", tmp_dir + "default_http", 
-#                            'HTTP_LOADBALANCER', vm_per_tier["vm-app-lb"]['ip'])
-#                     Put(vm['ip'], [tmp_dir + "default_http"] ).run()
-#                     SshProcess('mv /root/default_http /etc/apache2/site-available/', vm['ip']).run()
-#                 elif "vm-app" in vm['id']:
-#                     grep("conf_template/mysql.properties", tmp_dir + "mysql.properties", 
-#                'MYSQL_LOADBALANCER', vm_per_tier["vm-db-lb"]['ip'])
-#                     Put(vm['ip'], [tmp_dir + "mysql.properties"] ).run()
-#                     SshProcess('mv /root/mysql.properties /var/www/', vm['ip']).run()
-# 
-#             # Restarting services to take into account new configuration
-#             # 1. MySQL LB
-#             for vm in vm_per_tier["vm-db-lb"]:
-#                 SshProcess('haproxy -f /etc/haproxy/haproxy.cfg', vm['ip']).run()
-# 
-#             # 2. Tomcat
-#             for vm in vm_per_tier["vm-app"]:
-#                 SshProcess('/etc/init.d/tomcat6 restart', vm['ip']).run()
-# 
-#             # 3. Tomcat LB
-#             for vm in vm_per_tier["vm-app-lb"]:
-#                 SshProcess('/etc/init.d/apache2 restart', vm['ip']).run()
-# 
-#             # 4. HTTP
-#             for vm in vm_per_tier["vm-http"]:
-#                 SshProcess('/etc/init.d/apache2 restart', vm['ip']).run()
-# 
-#             # 5. HTTP LB
-#             for vm in vm_per_tier["vm-http-lb"]:
-#                 SshProcess('/etc/init.d/apache2 restart', vm['ip']).run()
-# 
-#             # Launch Client VM(s)
-#             vm_ids_client = []
-#             backing_files = []
-# 
-#             for i in range(1, self.nb_client + 1):
-#                 vm_ids_client.append("vm-client")
-#                 backing_files.append("/tmp/vm-client.img")
+            # Launching client
+            rubbos_stress = SshProcess(services['client']['launch_cmd'],
+                    map(lambda y: y['ip'], filter(lambda x: 'client' in x['id'],
+                                                  vms))[0])
+            rubbos_stress.ignore_exit_code = rubbos_stress.nolog_exit_code = True
+            rubbos_stress.start()
 
-#             logger.info(host + ': Installing VMS')
-#             install = install_vms(vms_client).run()
-#             if not install.ok:
-#                 logger.error(host + ': Unable to install the VMS  %s', slugify(comb))
-#                 exit()
+            sleep(self.stress_time)
+            rubbos_stress.kill()
+            # Gathering results
+            comb_dir = self.result_dir + '/' + slugify(comb) + '/'
+            try:
+                mkdir(comb_dir)
+            except:
+                logger.warning(thread_name + '%s already exists, ' + \
+                               'removing existing files', comb_dir)
+                for f in listdir(comb_dir):
+                    remove(comb_dir + f)
+
+            for service, conf in services.iteritems():
+                if conf['log_files']:
+                    print conf['log_files']
 # 
-#             boot_successfull = boot_vms_by_core(vms_client)
-#             if not boot_successfull:
-#                 logger.error(host + ': Unable to boot all the VMS for %s', slugify(comb))
-#                 exit()
-# 
-#             # Generate benchmark configuration file
-#             grep("conf_template/rubbos.properties", tmp_dir + "rubbos.properties.1", 
-#                  'HTTP_APACHE_SERVER', vm_per_tier["vm-http-lb"]['ip'])
-#             grep(tmp_dir + "rubbos.properties.1", tmp_dir + "rubbos.properties.2", 
-#                  'TOMCAT_SERVER', vm_per_tier["vm-app-lb"]['ip'])
-#             grep(tmp_dir + "rubbos.properties.2", tmp_dir + "rubbos.properties", 
-#                  'MARIADB_SERVER', vm_per_tier["vm-db-lb"]['ip'])
-# 
-#             # Upload benchmark configuration file
-#             vms_ip = [vm['ip'] for vm in vms_client]
-#             ChainPut(vms_ip, [ tmp_dir + "rubbos.properties" ] ).run()
-# 
-#             # Launch benchmark
-#             client_benchmark = []
-#             client_benchmark.append(TaktukRemote('cd /root/RUBBoS/Client/ 
-# &&  java -Xmx256m -Xms128m -server -classpath . edu.rice.rubbos.client.ClientEmulator', 
-#                                                      vms_ip))
-# 
-#             # Sleep for 10 minutes and kill the benchmark
-#             stress_actions = ParallelActions(client_benchmark)
-#             for p in stress_actions.processes:
-#                 p.ignore_exit_code = p.nolog_exit_code = True
-# 
-#             logger.info(host + ': Starting RUBBoS benchmark !! \n%s', pformat(client_benchmark) )
-#             stress_actions.start()
-#             for p in stress_actions.processes:
-#                 if not p.ok:
-#                     logger.error(host+': Unable to start the RUBBoS benchmark for combination %s', slugify(comb))
-#                     exit()
-# 
-#             sleep(self.stress_time)
-#             logger.info(host + ': Killing RUBBoS benchmark !!')
-#             stress_actions.kill()
-# 
-#             # Gathering results (to rewrite)
-#             comb_dir = self.result_dir + '/' + slugify(comb) + '/'
-#             try:
-#                 mkdir(comb_dir)
-#             except:
-#                 logger.warning(host + ': %s already exists, removing existing files', comb_dir)
-#                 for f in listdir(comb_dir):
-#                     remove(comb_dir + f)
+
 # 
 #             logger.info(host + ': Retrieving file from VMs')
 #             comb_dir = self.result_dir + '/' + slugify(comb) + '/'
@@ -443,7 +371,7 @@ class RuBBoS(vm5k_engine_para):
                 disks_present = False
                 break
         if disks_present:
-            logger.info('Disks %s are already present', pformat(disks))
+            logger.info('Disks %s are already present', ' '.join(disks))
         else:
             logger.info('Create backing file')
             setup._create_backing_file(disks=['/home/jorouzaudcornabas/VMs/'
