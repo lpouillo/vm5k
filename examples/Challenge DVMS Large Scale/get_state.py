bc3787959c1361c6d7ebeed626ddd27be40e70fb
Laurent Pouilloux <lolo@pouilloux.org>
2014-06-03 15:20:58 +0200

    [examples] major update of load_injector and get_state, which are now working as expected

--- examples/Challenge DVMS Large Scale/get_state.py
+++ examples/Challenge DVMS Large Scale/get_state.py
@@ -1,46 +1,68 @@
 #!/usr/bin/env python
 from pprint import pformat
-from xml.etree.ElementTree import Element, dump, SubElement
-from execo import logger, default_connection_params, sleep
+from xml.etree.ElementTree import Element, dump, SubElement, parse
+from execo import logger, default_connection_params, sleep, TaktukPut, TaktukRemote
 from execo_g5k import get_host_site, get_host_cluster, get_cluster_site
-from vm5k import list_vm
+from vm5k import default_vm
 from vm5k.utils import prettify
 
-default_connection_params['user'] = 'root'
+
 logger.setLevel('INFO')
-hosts = []
-with open('hosts.list') as fp:
-    for line in fp:
-        hosts.append(line.strip())
+default_connection_params['user'] = 'root'
 
-sites = list(set(map(get_host_site, hosts)))
-clusters =list(set(map(get_host_cluster, hosts)))
-
-state = Element('vm5k')
-for site in sites:
-    SubElement(state, 'site', attrib={'id': site})
-for cluster in clusters:
-    el_site = state.find("./site[@id='" + get_cluster_site(cluster) \
-                          + "']")
-    SubElement(el_site, 'cluster', attrib={'id': cluster})
-for host in hosts:
-    el_cluster = state.find(".//cluster/[@id='" + get_host_cluster(host) + "']")
-    SubElement(el_cluster, 'host', attrib={'id': host})                                               
+def _default_xml_value(key):
+    return default_vm[key] if key not in vm.attrib else vm.get(key)
 
 
+logger.info('Reading initial topo')
+tree = parse('final_topo.xml')
+state = tree.getroot()
 
+hosts = sorted([host.get('id') for host in state.findall('.//host')],
+                       key=lambda host: (host.split('.', 1)[0].split('-')[0],
+                                    int(host.split('.', 1)[0].split('-')[1])))
+logger.info('Pushing get_cpu_consumptions.rb on hosts')
+TaktukPut(hosts, ['get_cpu_consumptions.rb']).run()
+
+vms = []
+for host in state.findall('.//host'):
+    for vm in host.findall('.//vm'):
+        vms.append({'id': vm.get('id'),
+            'n_cpu': int(_default_xml_value('n_cpu')),
+            'cpuset': _default_xml_value('cpuset'),
+            'mem': int(_default_xml_value('mem')),
+            'hdd': int(_default_xml_value('hdd')),
+            'backing_file': _default_xml_value('backing_file'),
+            'host': host.get('id'),
+            'state': 'KO'})
+        
 while True:
-    logger.info('Listing VMS on %s', ','.join(map(lambda x: x.split('.')[0], hosts)))
-    hosts_vms = list_vm(hosts)
-    for host, vms in hosts_vms.iteritems():
-        el_host = state.find(".//host/[@id='" + host + "']")
-        for vm in vms: 
-            SubElement(el_host, 'vm', attrib={'id': vm['id']})
-    
-    f = open('topo.xml', 'w')
-    f.write(prettify(state))
-    f.close()
+    logger.debug('Cleaning all VMS from XML file')
     for el_host in state.findall('.//host'):
         for vm in el_host.findall('./vm'):
             el_host.remove(vm)
-        
+    logger.info('Retrieving VMS position and load ')
+    get_vms_load = TaktukRemote("ruby get_cpu_consumptions.rb",
+       hosts).run()
+    vms_loads = {}
+    hosts_vms = { host: [] for host in hosts}    
+    for p in get_vms_load.processes:
+        for line in p.stdout.strip().split('\n'):
+            tmp_load = line.split(' ')
+            vms_loads[tmp_load[0]] = float(tmp_load[1]) + float(tmp_load[2]) + float(tmp_load[-1])
+            hosts_vms[p.host.address].append(tmp_load[0])
+
+    for host, vms_list in hosts_vms.iteritems():
+        for vm in vms_list: 
+            attrib = filter(lambda x: x['id'] == vm, vms)[0]
+            attrib = dict(attrib.items() + {'load': str(vms_loads[vm])}.items())
+            attrib = {k: str(v) for k, v in attrib.items()}
+            del attrib['backing_file']
+            del attrib['host']
+            SubElement(el_host, 'vm', attrib=attrib)
+
+
+    f = open('state.xml', 'w')
+    f.write(prettify(state))
+    f.close()
+         
--- examples/Challenge DVMS Large Scale/load_injector.py
+++ examples/Challenge DVMS Large Scale/load_injector.py
@@ -2,34 +2,39 @@
 import threading
 from pprint import pprint
 import xml.etree.ElementTree as ET
-from execo import TaktukPut, TaktukRemote, logger, Remote
+from execo import Process, SshProcess, TaktukPut, TaktukRemote, logger, Remote, default_connection_params
 from execo.time_utils import sleep, Timer
+
 
 logger.setLevel('INFO')
 
+default_connection_params['user'] = 'root'
+
 vms = {}
-f = open('test_100/vms.list')
+f = open('vms.list')
 for line in f:
     ip, vm = line.strip().split('\t')
     vms[vm] = ip
 f.close()
-
+ 
 logger.info('Installing cpulimit on all VMs')
 apt = TaktukRemote('apt-get install -y cpulimit', vms.values()).run()
-
+ 
 logger.info('Copying memtouch on all vms')
-TaktukPut(vms.values(), ['memtouch-with-busyloop3']).run()
+copy_stress = TaktukPut(vms.values(), ['memtouch-with-busyloop3']).run()
 
+logger.info('Killing all memtouch processes') 
 kill_stress = TaktukRemote('killall memtouch-with-busyloop3', vms.values())
 for p in kill_stress.processes:
     p.shell = True
+    p.nolog_exit_code = ignore_exit_code = True
 kill_stress.run()
-
+ 
 logger.info('Starting memtouch process')
 cmd = './memtouch-with-busyloop3 --cmd-makeload ' +\
     '--cpu-speed 304408.621872 --mem-speed 63235516.087661 128 128'
 stress = TaktukRemote(cmd, vms.values()).start()
-
+ 
 all_started = False
 while not all_started:
     all_started = True
@@ -39,28 +44,70 @@ while not all_started:
             sleep(1)
             break 
 
+logger.info('Retrieving memtouch process id') 
 cmd = 'ps aux | grep "memtouch-with-busyloop3" | grep -v "grep" | awk \'{print $2}\''
 get_stress = TaktukRemote(cmd, vms.values()).run()
-processes = {}
+
+logger.info('Limiting memtouch to 1% cpu via cpulimit')
+tmp_vms = []
+processes = []
+vms_proc = {}
 for p in get_stress.processes:
-    vm = [vm for vm, ip in vms.items() if ip == p.host.address][0]
-    processes[vm] = int(p.stdout)
+    tmp_vms.append(p.host.address)
+    processes.append(p.stdout.strip())
+    vms_proc[p.host.address] = p.stdout.strip()
+init_limit = TaktukRemote('cpulimit -p {{processes}} -l 1', tmp_vms)
+for p in init_limit.processes:
+    p.nolog_exit_code = ignore_exit_code = True
+init_limit.start()
 
+logger.info('Configuring events generator')
+n_host = 0
+f = open('hosts.list')
+for line in f:
+    n_host += 1
+f.close()
+sed_time = Process('sed -i "s/simulator.duration.*/simulator.duration = ' + str(1000) + '/g"' + 
+      ' load_events_generator/config/simulator.properties').run()
+sed_nodes = Process('sed -i "s/nodes.number.*/nodes.number = ' + str(n_host) + '/g"' + 
+      ' load_events_generator/config/simulator.properties').run()
+sed_vms = Process('sed -i "s/vm.number.*/vm.number = ' + str(len(vms)) + '/g"' + 
+      ' load_events_generator/config/simulator.properties').run()
 
-tree = ET.parse('events_load.xml')
+logger.info('Generating events list')
+gen_events = Process('cd load_events_generator ; ' +
+      'java -jar load_events_generator.jar vms.list > ../events_load.xml')
+gen_events.shell = True
+gen_events.run()
+
+tree = ET.parse('events_load.xml') 
 root = tree.getroot()
-events_t = {}
+events = {}
 for event in root.findall('./event'):
-    events[int(event.get('time'))] = {'vm': event.get('target'),
+    events[int(round(float(event.get('time'))))] = {'vm': event.get('target'),
                                 'load': event.get('value')}
 
-def set_cpu_load(load, vm_ip, pid, ):
+ 
+def set_cpu_load(load, vm_ip, pid):
     """Use cpulimit to change process intensity on vm"""
-    kill_cpu_limit = SshProcess('ps aux| grep "cpulimit" | grep -v "grep" | awk \'{print $2}\' | xarg    s -r kill -9',
+    logger.info('kill cpu_limit on %s and set it to %s', vm_ip, load)
+    kill_cpu_limit = SshProcess('ps aux| grep "cpulimit" | grep -v "grep" | awk \'{print $2}\' | xargs -r kill -9',
                                 vm_ip).run()
-    start_cpu_limit = SshProcess('cpu')
+    start_cpu_limit = SshProcess('cpulimit -p ' + str(pid) + ' -l ' + str(load), vm_ip)
+    start_cpu_limit.nolog_exit_code = start_cpu_limit.ignore_exit_code = True
+    start_cpu_limit.start()
+ 
+ 
+logger.setLevel('INFO')
 
-
+ 
+timer = Timer()
+for e_time in sorted(events):
+    logger.info('sleeping %ss before setting load=%s on %s',
+        str(round(e_time-timer.elapsed())), events[e_time]['load'], events[e_time]['vm'])
+    sleep(e_time-timer.elapsed())
+    set_cpu_load(events[e_time]['load'], vms[events[e_time]['vm']], 
+                 vms_proc[vms[events[e_time]['vm']]]) 
 
 
 
