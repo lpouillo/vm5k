c84f87ef2e6da3aabb0c4bba9ff93610dda8fc81
Laurent Pouilloux <lolo@pouilloux.org>
2014-06-11 10:54:50 +0200

    [package] version 0.5 for G5K school

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='vm5k',
-    version='0.2.2',
+    version='0.5',
     author='Laurent Pouilloux',
     author_email='laurent.pouilloux@inria.fr',
     package_dir={'': 'src'},
