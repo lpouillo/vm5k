b63edf952da263546240f3187b34902eaba6c64c
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2014-10-28 01:11:52 +0100

    [package] version de dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='vm5k',
-    version='0.5.1',
+    version='0.5.2-dev',
     author='Laurent Pouilloux',
     author_email='laurent.pouilloux@inria.fr',
     package_dir={'': 'src'},
