d8d351687db4fec733267b0e770e1d795fae5dcb
Laurent Pouilloux <lolo@pouilloux.org>
2014-10-30 14:05:39 +0100

    [lib] services.aptcacher: code cleanign

--- src/vm5k/services/aptcacher.py
+++ src/vm5k/services/aptcacher.py
@@ -19,30 +19,30 @@ from execo import logger, TaktukRemote, Host, ParallelActions
 from execo.log import style
 
 
-def setup_aptcacher_server(hosts, base_dir = '/tmp/apt-cacher-ng'):
+def setup_aptcacher_server(hosts, base_dir='/tmp/apt-cacher-ng'):
     """Install and configure apt-cacher on one server"""
     hosts = map(Host, hosts)
-    logger.info('Installing apt-cacher on %s', 
-        ','.join([style.host(host.address) for host in hosts]))
+    logger.info('Installing apt-cacher on %s',
+                ','.join([style.host(host.address) for host in hosts]))
     logger.detail('Package')
-    package = TaktukRemote('export DEBIAN_MASTER=noninteractive ; apt-get update ; ' + \
-              'apt-get install -o Dpkg::Options::="--force-confdef" -o ' + \
-              'Dpkg::Options::="--force-confnew" -y apt-cacher-ng', 
-               hosts).run()
+    package = TaktukRemote('export DEBIAN_MASTER=noninteractive ; apt-get update ; ' +
+                           'apt-get install -o Dpkg::Options::="--force-confdef" -o ' +
+                           'Dpkg::Options::="--force-confnew" -y apt-cacher-ng',
+                           hosts).run()
     if not package.ok:
         logger.error('Unable to install apt-cacher-ng on %s')
         return
-    
-    logger.detail('Directory creation')    
-    log_dir   = base_dir + '/log'
+
+    logger.detail('Directory creation')
+    log_dir = base_dir + '/log'
     cache_dir = base_dir + '/cache'
-    mkdirs = TaktukRemote('mkdir -p ' + log_dir + '; mkdir -p ' + cache_dir + 
-                     '; chown -R apt-cacher-ng:apt-cacher-ng '+ base_dir,
-              hosts).run()
+    mkdirs = TaktukRemote('mkdir -p ' + log_dir + '; mkdir -p ' + cache_dir +
+                          '; chown -R apt-cacher-ng:apt-cacher-ng ' + base_dir,
+                          hosts).run()
     if not mkdirs.ok:
         logger.error('Unable to create the directories')
         return
-    
+
     configure = TaktukRemote('sed -i "s#/var/cache/apt-cacher-ng#' + cache_dir + 
                '#g" /etc/apt-cacher-ng/acng.conf ;' + 
                'sed -i "s#/var/log/apt-cacher-ng#' + log_dir + '#g" ' + 
@@ -55,9 +55,10 @@ def setup_aptcacher_server(hosts, base_dir = '/tmp/apt-cacher-ng'):
     if not configure.ok:
         logger.error('Unable to configure and restart the service')
         return
-    
+
     logger.info('apt-cacher-ng up and running on %s',
-        ','.join([style.host(host.address) for host in hosts]))
+                ','.join([style.host(host.address) for host in hosts]))
+
 
 def configure_apt_proxy(vms):
     """Override apt proxy-guess with server as proxy"""
@@ -70,9 +71,9 @@ def configure_apt_proxy(vms):
     for server, clients in hosts_vms.iteritems():
         server = Host(server)
         logger.detail('Configuring %s as APT proxy for %s',
-                  style.host(server.address), ','.join(clients))
-        conf.append(TaktukRemote(' echo \'Acquire::http::Proxy \"http://' + server.address + \
-            ':9999" ; \' > /etc/apt/apt.conf.d/proxy-guess', clients))
+                      style.host(server.address), ','.join(clients))
+        conf.append(TaktukRemote(' echo \'Acquire::http::Proxy \"http://' + 
+                                 server.address + ':9999" ; \' > /etc/apt/apt.conf.d/proxy-guess', 
+                                 clients))
     ParallelActions(conf).run()
-    
-    
+
