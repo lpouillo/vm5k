f983f1ad0a8feb6781333c8cbc8daefdf52c6a33
Laurent Pouilloux <lolo@pouilloux.org>
2015-07-01 17:04:28 +0200

    [lib] change color for step

--- src/vm5k/config.py
+++ src/vm5k/config.py
@@ -8,6 +8,6 @@ default_vm = {'id': None, 'host': None, 'ip': None, 'mac': None,
 configuration['color_styles']['OK'] = 'green',  'bold'
 configuration['color_styles']['KO'] = 'red', 'bold'
 configuration['color_styles']['Unknown'] = 'white', 'bold'
-configuration['color_styles']['step'] = 'on_magenta', 'bold'
+configuration['color_styles']['step'] = 'yellow', 'bold'
 configuration['color_styles']['VM'] = 'white', 'bold'
 configuration['color_styles']['Thread'] = 'cyan', 'bold'
